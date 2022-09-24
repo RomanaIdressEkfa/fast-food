cfe8435a780dfa6bb42c9a7d8dae3e939140c4ba
RomanaIdressEkfa <ekfaeshita@gmail.com>
2022-09-25 00:32:08 +0600

    dndjc

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Cards from './Pages/Cards';
 import Carousel from './Pages/Carousel';
 import Hero from './Pages/Hero';
 import Home from './Pages/Home';
+import Map from './Pages/Map';
 import Footer from './Pages/Shares/Footer';
 import Navbar from './Pages/Shares/Navbar';
 
@@ -14,6 +15,7 @@ function App() {
       <Cards></Cards>
       <Hero></Hero>
       <Carousel></Carousel>
+      <Map></Map>
       <Footer></Footer>
     </div>
   );
---
+++ src/Pages/Map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Map = () => {
+    return (
+        <div>
+            <iframe width="100%" height="600" id="gmap_canvas" src="https://maps.google.com/maps?q=uttara%206%20sector%20dhaka&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+        </div>
+    );
+};
+
+export default Map;
